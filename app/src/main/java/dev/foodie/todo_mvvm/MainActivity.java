b23bbee42857b433d05c9a67fbd77d70887d0abc
Foodie <olaorefouad565@gmail.com>
2019-09-17 11:47:34 +0100

    setup: initial commit

---
+++ app/src/main/java/dev/foodie/todo_mvvm/MainActivity.java
@@ -0,0 +1,14 @@
+package dev.foodie.todo_mvvm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
